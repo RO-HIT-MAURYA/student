e3d968fa90af7eaf8e0877c9ed738af00b62e9c8
RO-HIT-MAURYA <40297562+RO-HIT-MAURYA@users.noreply.github.com>
2020-05-06 12:05:59 +0530

    done

--- main.py
+++ main.py
@@ -9,21 +9,6 @@ row5 = list()
 row6 = list()
 row7 = list()
 
-r1 = [0.0]*7
-r2 = list()
-r3 = list()
-r4 = list()
-r5 = list()
-r6 = list()
-r7 = list()
-
-
-def get_distance(l1, l2):
-    #print(l1)
-    #print(l2)
-    r = math.sqrt(math.pow(l1[0]-l2[0], 2) + math.pow(l1[1] - l2[1], 2))
-    return r
-
 
 def main():
     csv_file = csv.reader(open('C:\\Users\\Rohit\\Downloads\\HIER.csv', 'r'))
@@ -32,24 +17,29 @@ def main():
         initialize_array(row, count)
         count = count + 1
         # print(row)
+    r1 = [0.0]*7
     print(r1)
 
-    fl = 0.0
-    fl = get_distance(row1, row2)
-    print(fl)
+    r2 = [get_distance(row1, row2), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    print(r2)
 
-    """""
-    print(row1)
-    print(row2)
-    print(row3)
-    print(row4)
-    print(row5)
-    print(row6)
-    print(row7)
-    """""
+    r3 = [get_distance(row1, row3), get_distance(row2, row3), 0.0, 0.0, 0.0, 0.0, 0.0]
+    print(r3)
 
+    r4 = [get_distance(row1, row4), get_distance(row2, row4), get_distance(row3, row4), 0.0, 0.0, 0.0, 0.0]
+    print(r4)
 
-    # print(distance.euclidean([9.2, 9.3, 0], [2.4, 2.5, 0], [8.9, 8.7, 0]))
+    r5 = [get_distance(row1, row5), get_distance(row2, row5), get_distance(row3, row5), get_distance(row4, row5), 0.0,
+          0.0, 0.0]
+    print(r5)
+
+    r6 = [get_distance(row1, row6), get_distance(row2, row6), get_distance(row3, row6), get_distance(row4, row6),
+          get_distance(row5, row6), 0.0, 0.0]
+    print(r6)
+
+    r7 = [get_distance(row1, row7), get_distance(row2, row7), get_distance(row3, row7), get_distance(row4, row7),
+          get_distance(row5, row7), get_distance(row6, row7), 0.0]
+    print(r7)
 
 
 def initialize_array(row, count):
@@ -85,6 +75,12 @@ def extract_data(row):
         count = count + 1
     #del al[0]
     return al
+
+
+def get_distance(l1, l2):
+    r = math.sqrt(math.pow(l1[0]-l2[0], 2) + math.pow(l1[1] - l2[1], 2) + math.pow(l1[2] - l2[2], 2) + math.pow(l1[3] - l2[3], 2) +
+                  math.pow(l1[4] - l2[4], 2) + math.pow(l1[5] - l2[5], 2))
+    return r
 
 
 if __name__ == '__main__':
