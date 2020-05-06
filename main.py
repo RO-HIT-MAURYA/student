10f0f712ec9007cc0d00838d798920806fa009e3
RO-HIT-MAURYA <40297562+RO-HIT-MAURYA@users.noreply.github.com>
2020-05-06 10:13:16 +0530

    formula is implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 row1 = list()
 row2 = list()
@@ -8,6 +9,21 @@ row5 = list()
 row6 = list()
 row7 = list()
 
+r1 = [0.0]*7
+r2 = list()
+r3 = list()
+r4 = list()
+r5 = list()
+r6 = list()
+r7 = list()
+
+
+def get_distance(l1, l2):
+    #print(l1)
+    #print(l2)
+    r = math.sqrt(math.pow(l1[0]-l2[0], 2) + math.pow(l1[1] - l2[1], 2))
+    return r
+
 
 def main():
     csv_file = csv.reader(open('C:\\Users\\Rohit\\Downloads\\HIER.csv', 'r'))
@@ -16,6 +32,13 @@ def main():
         initialize_array(row, count)
         count = count + 1
         # print(row)
+    print(r1)
+
+    fl = 0.0
+    fl = get_distance(row1, row2)
+    print(fl)
+
+    """""
     print(row1)
     print(row2)
     print(row3)
@@ -23,6 +46,9 @@ def main():
     print(row5)
     print(row6)
     print(row7)
+    """""
+
+
     # print(distance.euclidean([9.2, 9.3, 0], [2.4, 2.5, 0], [8.9, 8.7, 0]))
 
 
@@ -57,7 +83,7 @@ def extract_data(row):
         if count > 0:
             al.append(float(f))
         count = count + 1
-    del al[0]
+    #del al[0]
     return al
 
 
